9547c25d7bc330dd08af00ca9c6eeaccafaabcf1
BradV <47035560+BradVanDuker@users.noreply.github.com>
2022-07-22 21:40:02 -0600

    Add files via upload

--- Models/apiDataSource.py
+++ Models/apiDataSource.py
@@ -1,28 +1,71 @@
 from datetime import date, time, datetime
+from Models.configData import ConfigData
+import json
+from bs4 import BeautifulSoup
+
 import yaml
 import requests
 import ssl
 import json
+import urllib
 
 
 class APIDataSource:
     """
     This class interacts with the government's API
     """
+    _website = 'https://api.govinfo.gov'
+    _API_key = ConfigData.get_config_dict()['API_key']
+    _does_not_exist_message = 'The requested resource does not exist.'
 
-    def __init__(self):
-        self._website = 'https://api.govinfo.gov'
-        self._CONFIG_FILE = "E:\\Users\\Brad\\Documents\\billspy.config.yaml"
-        self._API_key = None
-        with open(self._CONFIG_FILE) as f:
-            configuration = yaml.safe_load(f)
-            self._API_key = configuration['API_key']
+    @classmethod
+    def store(cls, bill_id, text, is_metadata=False):
+        raise NotImplementedError("API can't store data")
 
-    def get_bills_listing(self, start_date: date, end_date: date):
+    @classmethod
+    def get(cls, bill_id, is_metadata=False):
+        # get the package link
+        # get the bill text link from the dict
+        package = json.loads(cls._get_package_data(bill_id))
+        if is_metadata:
+            return package
+        else:
+            url = package['download']['txtLink']
+            html = cls._execute_query(url)
+            soup = BeautifulSoup(html, 'html.parser')
+            text = soup.text
+            return text
+
+    @classmethod
+    def does_bill_exist(cls, bill_id, is_metadata=False):
+        package = cls._get_package_data(bill_id)
+        is_there_a_message = 'message' in package
+        return not (is_there_a_message and cls._does_not_exist_message in package['message'])
+
+    @classmethod
+    def yield_bill_ids(cls, star_date, end_date):
+        packages = cls._get_list_of_packages(star_date, end_date)
+        for package in packages:
+            yield package['packageId']
+
+    @classmethod
+    def _get_dict_from_link(cls, url):
+        data = cls._execute_query(url)
+        dictionary = json.loads(data)
+        return dictionary
+
+    @classmethod
+    def _get_package_data(cls, bill_id):
+        url = f'{cls._website}/packages/{bill_id}/summary'
+        package = cls._execute_query(url)
+        return package
+
+    @classmethod
+    def _get_list_of_packages(cls, start_date: date, end_date: date):
         iso_midnight_str = 'T00:00:00Z'  # might need to replace : with %3A
-        #start = start_date.isoformat()
-        #start = start.join(iso_midnight_str)
-        #end = end_date.isoformat().join(iso_midnight_str)
+        # start = start_date.isoformat()
+        # start = start.join(iso_midnight_str)
+        # end = end_date.isoformat().join(iso_midnight_str)
         midnight = time(0, 0, 0)
         start = datetime.combine(start_date, midnight).isoformat()
         end = datetime.combine(end_date, midnight).isoformat()
@@ -30,78 +73,52 @@ class APIDataSource:
         collection = 'BILLS'
         page_size = 100
         offset = 0
-        all_bills = []
+        all_packages = []
 
         while True:
-
-            url = f'{self._website}/collections/{collection}/{start}Z/{end}Z?offset={offset}&pageSize={page_size}'
+            url = f'{cls._website}/collections/{collection}/{start}Z/{end}Z?offset={offset}&pageSize={page_size}'
+            # url2 = r'https://api.govinfo.gov/collections/BILLS/2022-02-02T00:00:00Z/2022-02-03T00:00:00Z?offset=0&pageSize=100'
+            # print(url == url2)
             # print(url)
-            self._append_api_key(url)
-            result = self._execute_query(url)
+            result = cls._execute_query(url)
             data = json.loads(result)
-            print(data.keys())
-            # print(data['validationMessages'])
+
+            if data['message'] == 'No results found':
+                break
+
             packages = data['packages']
-            all_bills.extend(packages)
+            all_packages.extend(packages)
 
             if data['nextPage'] is None:
                 break
 
             offset = offset + page_size
 
-        return all_bills
+        return all_packages
 
+    def _get_bill_text(self, bill_id):
+        url = f'{self._website}/packages/{bill_id}/htm'
+        url = self._append_api_key(url)
+        return self._execute_query(url)
 
-    def _append_api_key(self, url):
+    def _get_bill_pdf(self, bill_id):
+        url = f'https://www.govinfo.gov/content/pkg/{bill_id}/pdf/{bill_id}.pdf'
+        return self._execute_query(url)
+
+    @classmethod
+    def _append_api_key(cls, url):
         new_url = url
         if '?' not in new_url:
             #new_url = ''.join(new_url, '?')
             new_url += '?'
-        new_url = new_url + f'&api_key={self._API_key}'
+        new_url = new_url + f'&api_key={cls._API_key}'
         return new_url
         # return new_url(f'&api_key={self._API_key}')
 
-    def _execute_query(self, url: str) -> str:
-        url = self._append_api_key(url)
-        print(url)
-        # response = urllib.request.urlopen(url)
-        # data = response.read()
-        # text = data.decode('utf8', 'ignore')
-
-        # This line is for testing purposes -- To be deleted when I've got things working
-        # url = 'https://api.govinfo.gov/collections?api_key=AlGT2EfGtbvovEqIWBjQC2ubL8Xj3rsdSBduXISa'
-
+    @classmethod
+    def _execute_query(cls, url: str) -> str:
+        url = cls._append_api_key(url)
+        # print(url)
         response = requests.get(url)
-        print(response.content)
         return response.text
 
-        #
-        # headers = {
-        #     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-        #     "Accept-Encoding": "gzip, deflate",
-        #     "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8",
-        #     "Dnt": "1",
-        #     "Host": "httpbin.org",
-        #     "Upgrade-Insecure-Requests": "1",
-        #     # "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36",
-        #     # 'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 6.1; rv:2.2) Gecko/20110201'
-        #     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0'
-        # }
-        #
-        # data = ''
-        # try:
-        #     # Gives 404 Not Found error if including headers.  Otherwise, gives a 400 Bad Request Error
-        #     request = urllib.request.Request(url, data=None, headers=headers)
-        #
-        #     # to ignore ssl certificate errors.  Gives a 404 Not Found error
-        #     ctx = ssl.create_default_context()
-        #     ctx.check_hostname = False
-        #     ctx.verify_mode = ssl.CERT_NONE
-        #     html = urllib.request.urlopen(request, context=ctx).read()
-        #
-        #     response = urllib.request.urlopen(request)
-        #     page = response.read()
-        #     data = page.decode('utf8', 'ignore')
-        # except urllib.error.HTTPError as e:
-        #     print(e)
-        # return data
---
+++ Models/dataSource.py
@@ -0,0 +1,34 @@
+from os import path
+
+from Models.apiDataSource import APIDataSource
+from Models.fileDataSource import FileDataSource
+
+
+class DataSource:
+    def __init__(self):
+        self.api_data_source = APIDataSource()
+        self.file_data_source = FileDataSource()
+
+    def yield_list_of_bill_ids(self, start_date, end_date):
+        return self.api_data_source.yield_bill_ids(start_date, end_date)
+
+    def get(self, bill_id: str, to_cache=False) -> str | None:
+        # First try to get the text from file.  If it doesn't exist, try the gov's api
+        if self.file_data_source.does_bill_exist(bill_id):
+            return self.file_data_source.get(bill_id)
+        else:
+            text = self.api_data_source.get(bill_id)
+            if to_cache:  # if we're caching, write it to file
+                self.file_data_source.store(bill_id, text)
+            return text
+
+    def store(self, bill_id, text: str, is_metadata=False):
+        self.file_data_source.store(bill_id, text, is_metadata)
+
+    def get_cached_bill_list(self, is_metadata=False):
+        return self.file_data_source.get_bill_ids(is_metadata=is_metadata)
+
+    def cache_bill(self, bill_id: [str]):
+        if not self.file_data_source.does_bill_exist(bill_id):
+            text = self.api_data_source.get(bill_id)
+            self.file_data_source.store(bill_id, text)
